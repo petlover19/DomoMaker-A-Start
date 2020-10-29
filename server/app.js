4a64ddf42e1fd183245c9e03c17e79317ef39582
varpu19 <vap8665@rit.edu>
2020-10-29 19:23:27 -0400

    tweak

--- server/app.js
+++ server/app.js
@@ -6,43 +6,44 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
-const router = require('./router.js');
+
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/simpleModelsHW';
+const dbURL = process.env.MONGODB_URI || 'mongodb+srv://WebDevII:Smalus18@cluster0.cfff2.mongodb.net/doma';
 
 const mongooseOptions = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
 };
 
 mongoose.connect(dbURL, mongooseOptions, (err) => {
-  if (err) {
-    console.log('Could not connect to database');
-    throw err;
-  }
+    if (err) {
+        console.log('Could not connect to database');
+        throw err;
+    }
 });
 
+const router = require('./router.js');
+
 const app = express();
-app.use('/assets', express.static(path.resolve(`${__dirname}/../client/`)));
+app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
+app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.use(compression());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.engine('handlebars', expressHandlebars({
-  defaultLayout: '',
+    defaultLayout: 'main',
 }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
-app.use(favicon(`${__dirname}/../client/img/favicon.png`));
 app.use(cookieParser());
 
 router(app);
 
 app.listen(port, (err) => {
-  if (err) {
-    throw err;
-  }
-  console.log(`Listening on port ${port}`);
+    if (err) {
+        throw err;
+    }
+    console.log(`Listening on port ${port}`);
 });
